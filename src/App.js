c5fef5e22b09341fe56b6781b058ec2d8f89823c
Revati <revati.v.bordawekar@gmail.com>
2023-01-07 22:56:01 -0800

    added custom hook and Navbar template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Search from './pages/search/Search'
@@ -9,10 +10,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter >
+      <Navbar />
        <Routes>
           <Route exact path='/' element={<Home />} /> 
           <Route path='/create' element={<Create />} />
-          <Route path='/recipe' element={<Recipe />}/>
+          <Route path='/recipes/:id' element={<Recipe />}/>
           <Route path='/search' element={<Search />} />       
       </Routes>
       
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
+import {useFetch} from '../../hooks/useFetch'
+
 import './Home.css'
 import React from 'react'
+import RecipeList from '../../components/RecipeList'
 
 function Home() {
+
+  const {data, isPending, error } = useFetch('http://localhost:3000/recipes')
   return (
-    <div>Home</div>
+    <div className='home'>
+
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+
+      {data && <RecipeList recipes = {data} />}
+
+    </div>
   )
 }
 
