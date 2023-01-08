5c055d223d278f7c5f12eab5e01b731443f5806e
Revati <revati.v.bordawekar@gmail.com>
2023-01-08 07:48:13 -0800

    create recipe form

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,10 +1,59 @@
 import './Create.css'
+import {useState} from 'react';
 
 import React from 'react'
 
 function Create() {
+
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+
+  }
+
   return (
-    <div>Create</div>
+    <div className='create'>
+      <h2 className='page-title'>Add a new recipe</h2>
+      
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input 
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+          />
+        </label>
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea 
+          onChange={(e) => setMethod(e.target.value)}
+          value={method}
+          required
+          />
+        </label>
+
+        {/* ingredients */}
+
+        <label>
+          <span>Cooking Time(minutes):</span>
+          <input 
+          type="number"
+          onChange={(e) => setCookingTime(e.target.value)}
+          value={cookingTime}
+          required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+      </form>
+    </div>
   )
 }
 
