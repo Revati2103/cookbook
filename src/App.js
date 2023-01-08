a8854aa47607c2b41df3c0258c0cd0ec89767b4f
Revati <revati.v.bordawekar@gmail.com>
2023-01-07 18:40:08 -0800

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter >
+       <Routes>
+          <Route exact path='/' element={<Home />} /> 
+          <Route path='/create' element={<Create />} />
+          <Route path='/recipe' element={<Recipe />}/>
+          <Route path='/search' element={<Search />} />       
+      </Routes>
       
+      </BrowserRouter>
     </div>
   );
 }
