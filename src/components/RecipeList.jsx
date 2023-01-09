3c0325316a2a125c4a8a87eb004e26b410ca2570
Revati <revati.v.bordawekar@gmail.com>
2023-01-09 06:35:39 -0800

    all functionality working

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function RecipeList({recipes}) {
+  if(recipes.length === 0){
+    return <div className='error'>No recipes found with that keyword...</div>
+  }
   return (
     <div className='recipe-list'>
         {recipes.map(recipe => (
